88cbd3a78bff0e9c64fdfb570554734c73db474b
Majd833 <lina.azzam.1611@gmail.com>
2024-09-16 19:09:50 +0300

    hw

--- hw.py
+++ hw.py
@@ -1,5 +1,5 @@
-temp=int(input("Enter the temperature in Fehrenheight:"))
-if (temp>90):
+temp=int(input("Enter the temperature in Fehrenheight"))
+if (temp<90):
     print("Put on some cotton clothes")
 else:
     print("Put on normal clothes")
